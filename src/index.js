bbe156a37a928766e7f35dc54f52e7ccedc5e900
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-07-26 18:19:35 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,16 +4,38 @@ import ReactDOM from 'react-dom/client';
 function App() {
   return (
     <div>
-      <h1>Hello, World!</h1>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+const Header = () => {
+  return (
+    <h1>Fast Pizza Co.</h1>
+  );
+}
+
+const Menu = () => {
+  return (
+    <div>
+      <h2>Our Menu</h2>
       <Pizza />
     </div>
+  );
+}
+
+const Footer = () => {
+  return (
+    <footer>All rights reserved &copy; {new Date().getFullYear()}</footer>
   );
 }
 
 function Pizza() {
   return (
     <div>
-    <img src='pizzas/prosciutto.jpg' alt='Pizza Prosciutto' />
+      <img src='pizzas/prosciutto.jpg' alt='Pizza Prosciutto' />
       <h2>Pizza Prosciutto</h2>
       <p>Tomato, mozarella, ham, aragula, and burrata cheese</p>
     </div>
