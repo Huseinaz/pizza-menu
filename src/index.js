aee861748265efcf13349b0a32682bb9a691703c
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-07-31 18:32:43 +0300

    ? and :

--- src/index.js
+++ src/index.js
@@ -66,15 +66,23 @@ const Header = () => {
 }
 
 const Menu = () => {
+  const pizzas = pizzaData;
+  // const pizzas = [];
+  const numPizzas = pizzas.length;
+
   return (
     <main className='menu'>
       <h2>Our Menu</h2>
 
-      <ul className='pizzas'>
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
-        ))}
-      </ul>
+      {numPizzas > 0 ? (
+        <ul className='pizzas'>
+          {pizzaData.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      ) : (
+        <p>We're still working on our menu. PLease come back later :)</p>
+      )}
 
     </main>
   );
@@ -102,11 +110,13 @@ const Footer = () => {
 
   return (
     <footer className='footer'>
-      {isOpen && (
+      {isOpen ? (
         <div className='order'>
           <p>We're open until {closeHour}:00, Come visit us or order online.</p>
           <button className='btn'>Order</button>
         </div>
+      ) : (
+        <p>We're happy to welcome you between {openHour}:00 and {closeHour}:00.</p>
       )}
       {/* All rights reserved &copy; {new Date().getFullYear()} */}
     </footer>
